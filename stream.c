28b5e05affc87287873e2c9c1e9a1b92dfdb58c7
James Mastros <james@mastros.biz>
2010-04-21 17:24:10 +0100

    glk_put_char_stream

--- stream.c
+++ stream.c
@@ -13,12 +13,12 @@ glui32 glk_get_buffer_stream(strid_t str, char *buf, glui32 len) {
 
 /* http://www.eblong.com/zarf/glk/glk-spec-070_5.html section 5.4 */
 void glk_stream_set_position(strid_t str, glsi32 pos, glui32 seekmode) {
-  (*str->vtable.set_position)(str, pos, seekmode);
+  (*str->vtable->set_position)(str, pos, seekmode);
 }
 
 /* http://www.eblong.com/zarf/glk/glk-spec-070_5.html#s.4 */
 glui32 glk_stream_get_position(strid_t str) {
-  return ((*str->vtable.get_position)(str));
+  return ((*str->vtable->get_position)(str));
 }
 
 /* http://www.eblong.com/zarf/glk/glk-spec-070_5.html#s.1 */
@@ -29,4 +29,9 @@ void glk_put_char_uni(glui32 ch) {
 void glk_put_char(unsigned char ch) {
   glui32 ch_uni = ch;
   glk_put_char_uni(ch_uni);
+}
+
+void glk_put_char_stream(strid_t str, unsigned char ch) {
+  glui32 ch_uni = ch;
+  glk_put_char_stream_uni(current_stream, ch);
 }
