0894dae3375911bc073a730a0d6c8bf730dcc50a
Jess Robinson <castaway@desert-island.me.uk>
2010-05-07 20:13:55 +0100

    Support indication of type of input required, under javascripty

--- perl/root/js/next-action.js
+++ perl/root/js/next-action.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function(){
         jQuery("#input").submit(function() {
                 var fields = jQuery("#input").serialize();
-                alert(fields);
                 jQuery.get("/game/continue", fields, function(data) {
-                        alert(data);
                         jQuery("#"+data.winid).append(data.content);
                         jQuery("#"+data.winid).scrollTop(jQuery("#"+data.winid).height - 400);
+                        jQuery('#prompt_type').text(data.input_type);
+                        jQuery('#input_type').val(data.input_type);
                     });
                 return false;
             });
