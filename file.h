59347d67f835cc404303200e92322e5c28d90b70
James Mastros <james@mastros.biz>
2010-05-09 17:44:39 +0100

    glk_stream_open_file, writing to file streams.  FIXME: What other things did this break?

--- file.h
+++ file.h
@@ -8,4 +8,12 @@ struct glk_fileref_struct {
   gidispatch_rock_t dispatch_rock;
 };
 
+/* Keep these synced to the function definitions in file.c */
+#define USAGE_NAME_LEN strlen("InputRecord, BinaryMode")+1
+extern void usage_to_name(glui32 usage, char *name);
+
+#define FILEMODE_NAME_LEN strlen("WriteAppend")+1
+extern void filemode_to_name(glui32 filemode, char *name);
+
+
 #endif
