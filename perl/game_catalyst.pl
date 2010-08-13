f64d5f80f2d1ebd9abcfdd4ae30267c90d250c95
James Mastros <james@mastros.biz>
2010-08-13 21:09:33 +0100

    Change about the frontpage

--- perl/game_catalyst.pl
+++ perl/game_catalyst.pl
@@ -11,6 +11,7 @@ my $c = {
            title => 'A Tight Spot',
            link => 'http://ifdb.tads.org/viewgame?id=rsjzz9w60k6o6od4',
            nsfw => 0,
+           desc => 'An implementation of sokoban, a box-pushing puzzle game.  Not, strictly speaking, interactive fiction, but an interesting technology demonstration.',
         },
         advent => {
             vm    => 'glulx',
@@ -19,6 +20,7 @@ my $c = {
             restore => [["line", "restore\n"]],
             link => 'http://en.wikipedia.org/wiki/Adventure_(computer_game)',
             nsfw => 0,
+            desc => 'The original computer game, Adventure.  It simulates exploring a cave in Kentucky... with elves and a troll.'
           },
         'blue-lacuna' => {
             vm => 'glulx',
@@ -27,6 +29,7 @@ my $c = {
             restore => [["char", "r"]],
             link => 'http://www.lacunastory.com/about.html',
             nsfw => 0,
+            desc => 'Perhaps the most stressing of the <b>fiction</b> in interactive fiction of any game here, Blue Lacuna has actual characters and plot, in addition to puzzles.  The primary author of glknew.org considers this his favorite game here.',
         },
         alabaster     => {
             vm => 'glulx',
@@ -38,6 +41,7 @@ my $c = {
                         ["line", "restore\n"]],
             link => 'http://emshort.home.mindspring.com/Alabaster/index.html',
             nsfw => 0,
+            desc => 'An interesting, and rather spooky, take on the classic tale of Snow White.  Primarily takes the form of a dialog, between Snow White, and the man sent to kill her.',
         },
         acg           => {
             vm => 'glulx',
@@ -46,6 +50,7 @@ my $c = {
             restore => [["line", "restore\n"]],
             link => 'http://ifdb.tads.org/viewgame?id=snk6qx8hfn3xpm0a',
             nsfw => 0,
+            desc => 'A rather amusing puzzle game, in which you are sent to test a number of pieces of equipment as an author for the Adventurer\'s Consumer Guide, by being sent out with nothing else, and told to bring back a treasure.  Features lots of amusing text, and inventive puzzles.',
         },
         king          => {
             vm => 'glulx',
@@ -54,6 +59,7 @@ my $c = {
             restore => [["char", "r"]],
             link => 'http://maher.filfre.net/King/',
             nsfw => 0,
+            desc => 'A tale of the Cuthulu mythos, set in Black Death era London.  Features several playwrights of note.  Dark and mysterious, a good play.'
         },
         curses        => {
             vm => 'z-code',
@@ -62,7 +68,8 @@ my $c = {
             link => 'http://www.sparkynet.com/spag/c.html#curses',
             nsfw => 0,
             restore => [['char', ' '],
-                        ['line', 'restore']]
+                        ['line', 'restore']],
+            desc => 'Another funny one.  In Curses, you have escaped the preparations for a family vacation, ostensibly to get a tourist map of Paris from your attic -- which, it turns out, is quite extensive.',
         },
         zork1         => {
             vm => 'z-code',
@@ -70,6 +77,7 @@ my $c = {
             title => 'Zork I',
             link => 'http://en.wikipedia.org/wiki/Zork_I',
             nsfw => 0,
+            desc => 'A classic of the genre, Zork is an Infocom game that was freed as a promotion for Legends of Zork, a browser-based MMORPG.  A, perhaps <b>the</b> classic treasure hunt.',
         },
         emy           => {
             vm => 'agt',
@@ -94,6 +102,7 @@ my $c = {
             link => 'http://www.ifwiki.org/index.php/Earl_Grey',
             restore => [[line => "restore\n"]],
             nsfw => 0,
+            desc => 'An odd little pun-based game.'
         },
         british_fox => {
             vm => 'taf',
@@ -102,27 +111,28 @@ my $c = {
             link => 'http://www.ifwiki.org/index.php/British_Fox_and_the_Celebrity_Abductions',
             nsfw => 1,
         },
-        bearg => {
-            vm => 'z-code',
-            location => 'bearg.z5',
-            title => 'Ein Bär geht aus',
-            link => 'http://www.if-album.menear.de/pages/fantasy.html#baer',
-            nsfw => 0,
-        },
-        banana => {
-            vm => 'glulx',
-            location => 'bnareplk.ulx',
-            title => 'Banana Republic [de]',
-            link => 'http://www.if-album.menear.de/pages/humor.html#bananenrepublik',
-            nsfw => 0,
-        },
-        Absturzmomente => {
-            vm => 'z-code',
-            location => 'abstmom.zblorb',
-            title => 'Absturzmomente',
-            link => 'http://ifdb.tads.org/viewgame?id=r1ayxo348ij7uk8p',
-            nsfw => 0,
-        },
+#         bearg => {
+#             vm => 'z-code',
+#             location => 'bearg.z5',
+#             title => 'Ein Bär geht aus',
+#             link => 'http://www.if-album.menear.de/pages/fantasy.html#baer',
+#             nsfw => 0,
+                  
+#         },
+#         banana => {
+#             vm => 'glulx',
+#             location => 'bnareplk.ulx',
+#             title => 'Banana Republic [de]',
+#             link => 'http://www.if-album.menear.de/pages/humor.html#bananenrepublik',
+#             nsfw => 0,
+#         },
+#         Absturzmomente => {
+#             vm => 'z-code',
+#             location => 'abstmom.zblorb',
+#             title => 'Absturzmomente',
+#             link => 'http://ifdb.tads.org/viewgame?id=r1ayxo348ij7uk8p',
+#             nsfw => 0,
+#         },
     },
 
     interpreters => {
